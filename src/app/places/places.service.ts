e6572d96918298783049831032d05f9afd0033d8
Luis <luisfelipe@feyh.com.br>
2025-01-23 14:49:27 -0300

    fdjgdfgj

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,20 +1,42 @@
-import { Injectable, signal } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
 import { Place } from './place.model';
+import { map, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
-  loadAvailablePlaces() {}
+  loadAvailablePlaces() {
+    return this.fetchPlaces(
+      'http://localhost:3000/places',
+      'Something went wrong feching the available places. Please try again later.'
+    );
+  }
 
   loadUserPlaces() {}
 
-  addPlaceToUserPlaces(place: Place) {}
+  addPlaceToUserPlaces(placeId: Place) {
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId,
+      })
+  }
 
   removeUserPlace(place: Place) {}
+
+  private fetchPlaces(url: string, errorMessage: string) {
+    return this.httpClient.get<{ places: Place[] }>(url).pipe(
+      map((resData) => resData.places),
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
 }
